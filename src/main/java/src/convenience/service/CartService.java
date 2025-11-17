a9e87e83e107c6ce720f281540a9a341297ec957
윤민수 <koelysa5696@naver.com>
2025-11-17 18:25:34 +0900

    feat: 카트서비스 구현
    - 카트 조회
    - 카트에 추가 및 제거하는 흐름

---
+++ src/main/java/src/convenience/service/CartService.java
@@ -0,0 +1,39 @@
+package src.convenience.service;
+
+import java.util.Map;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import src.convenience.domain.cart.Cart;
+import src.convenience.domain.entity.Product;
+import src.convenience.domain.entity.ProductRepository;
+
+@Service
+public class CartService {
+    private final ProductRepository productRepository;
+    private final Cart cart;
+
+
+    public CartService(ProductRepository productRepository, Cart cart) {
+        this.productRepository = productRepository;
+        this.cart = cart;
+    }
+
+    @Transactional
+    public void addItem(Long productId, Integer quantity) {
+        Product product = productRepository.getByIdOrThrow(productId);
+
+        int totalQuantity = cart.currentQuantity(productId) + quantity;
+        product.checkQuantity(totalQuantity);
+
+        cart.addItem(productId, quantity);
+    }
+
+    public void removeItem(Long productId) {
+        cart.removeItem(productId);
+    }
+
+    public Map<Long, Integer> getCartItems() {
+        return cart.getItems();
+    }
+
+}
