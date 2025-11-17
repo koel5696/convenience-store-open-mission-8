62f3f499fd19c077b6d740157d7501697de29d19
윤민수 <koelysa5696@naver.com>
2025-11-17 18:15:42 +0900

    feat: 상품 목록 파일 로더 생성
    - 애플리케이션 시작 시 products.csv 파일을 읽어 초기 상품 데이터를 DB에 저장하는 기능 추가

---
+++ src/main/java/src/convenience/config/CsvProductDataLoader.java
@@ -0,0 +1,45 @@
+package src.convenience.config;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+import src.convenience.domain.entity.Product;
+import src.convenience.domain.entity.ProductRepository;
+
+import java.util.List;
+
+@Component
+public class CsvProductDataLoader implements ApplicationRunner {
+
+    private final ProductRepository productRepository;
+
+    public CsvProductDataLoader(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        if (productRepository.count() > 0) return; // 중복 방지
+
+        try (BufferedReader br = new BufferedReader(
+                new InputStreamReader(
+                        getClass().getResourceAsStream("/products.csv")
+                ))) {
+
+            List<Product> products = br.lines()
+                    .skip(1)
+                    .map(line -> line.split(","))
+                    .map(tokens -> new Product(
+                            tokens[0],
+                            Integer.parseInt(tokens[1]),
+                            Integer.parseInt(tokens[2]),
+                            tokens.length > 3 && !tokens[3].isBlank() ? tokens[3] : null
+                    ))
+                    .toList();
+
+            productRepository.saveAll(products);
+        }
+    }
+}
