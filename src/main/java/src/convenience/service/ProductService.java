e197a6bbe97ee4f6c2edd401258aa76162989824
윤민수 <koelysa5696@naver.com>
2025-11-17 18:25:57 +0900

    feat: 상품서비스 구현
    - 상품 정보를 읽어옴

---
+++ src/main/java/src/convenience/service/ProductService.java
@@ -0,0 +1,23 @@
+package src.convenience.service;
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import src.convenience.domain.entity.ProductRepository;
+import src.convenience.dto.product.ProductResponse;
+
+@Service
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public List<ProductResponse> getAllProducts() {
+        return productRepository.findAll().stream()
+                .map(ProductResponse::new)
+                .toList();
+    }
+}
