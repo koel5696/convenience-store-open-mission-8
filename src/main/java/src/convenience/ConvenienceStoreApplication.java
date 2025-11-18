6a31620bce245c3285b2bae0ac6a1a693bc8c96d
윤민수 <koelysa5696@naver.com>
2025-11-19 00:01:03 +0900

    feat: 어플리케이션 시작 main 구현

---
+++ src/main/java/src/convenience/ConvenienceStoreApplication.java
@@ -0,0 +1,13 @@
+package src.convenience;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvenienceStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConvenienceStoreApplication.class, args);
+    }
+
+}
