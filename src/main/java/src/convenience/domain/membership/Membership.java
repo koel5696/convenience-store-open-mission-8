6f0e0be23d46d00be37bcba08ba05777ae0ead81
윤민수 <koelysa5696@naver.com>
2025-11-18 23:46:37 +0900

    refactor: promotion이 없는 상품 구분 호출 방식 변경

--- src/main/java/src/convenience/domain/membership/Membership.java
+++ src/main/java/src/convenience/domain/membership/Membership.java
@@ -2,7 +2,6 @@ package src.convenience.domain.membership;
 
 import java.util.List;
 import org.springframework.stereotype.Component;
-import src.convenience.domain.promotion.Promotion;
 import src.convenience.dto.payment.PayRequest;
 import src.convenience.dto.receipt.ReceiptResponse.ReceiptItem;
 
@@ -10,11 +9,6 @@ import src.convenience.dto.receipt.ReceiptResponse.ReceiptItem;
 public class Membership {
     private static final int DISCOUNT_PERCENT = 10;
     private static final int MAX_DISCOUNT_AMOUNT = 8000;
-    private final Promotion promotion;
-
-    public Membership(Promotion promotion) {
-        this.promotion = promotion;
-    }
 
     public int discount(PayRequest request, List<ReceiptItem> items) {
         if (Boolean.TRUE.equals(request.membership())) {
@@ -27,7 +21,7 @@ public class Membership {
 
     private int calculateNonPromotionAmount(List<ReceiptItem> items) {
         return items.stream()
-                .filter(item -> promotion.checkNonePromotion(item.promotion()))
+                .filter(ReceiptItem::isNonPromotion)
                 .mapToInt(item -> item.price() * item.paidQuantity())
                 .sum();
     }
