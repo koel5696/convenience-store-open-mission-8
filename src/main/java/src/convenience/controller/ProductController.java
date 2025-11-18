9e9662a3fc3f4fceaa705c3a3edf0538d976148a
윤민수 <koelysa5696@naver.com>
2025-11-18 23:58:04 +0900

    feat: 상품 목록 Controller

---
+++ src/main/java/src/convenience/controller/ProductController.java
@@ -0,0 +1,21 @@
+package src.convenience.controller;
+
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import src.convenience.dto.product.ProductResponse;
+import src.convenience.service.productService.ProductService;
+
+@RestController
+public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/api/products")
+    public List<ProductResponse> getProductList() {
+        return productService.getAllProducts();
+    }
+}
