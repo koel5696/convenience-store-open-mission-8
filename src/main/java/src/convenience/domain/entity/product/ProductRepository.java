1c249ecabf055b8785d4c558aa264e5834f8fda7
윤민수 <koelysa5696@naver.com>
2025-11-19 22:36:58 +0900

    feat: 상품 이름으로 찾는 로직 구현

--- src/main/java/src/convenience/domain/entity/product/ProductRepository.java
+++ src/main/java/src/convenience/domain/entity/product/ProductRepository.java
@@ -1,10 +1,18 @@
 package src.convenience.domain.entity.product;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<Product> findByName(String name);
+
     default Product getByIdOrThrow(Long id) {
         return findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("상품 없음: " + id));
+    }
+
+    default Product getByNameOrThrow(String name) {
+        return findByName(name)
+                .orElseThrow(() -> new IllegalArgumentException("상품 없음: " + name));
     }
 }
