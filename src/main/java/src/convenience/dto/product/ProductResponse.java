9e6542224b2787f29810738817c7bd3c1de2d101
윤민수 <koelysa5696@naver.com>
2025-11-19 22:38:49 +0900

    feat: 프로모션 날짜 dto 멤버 추가

--- src/main/java/src/convenience/dto/product/ProductResponse.java
+++ src/main/java/src/convenience/dto/product/ProductResponse.java
@@ -1,9 +1,24 @@
 package src.convenience.dto.product;
 
+
 import src.convenience.domain.entity.product.Product;
 
-public record ProductResponse(Long id, String name, int price, int quantity, String promotion) {
-    public ProductResponse(Product product) {
-        this(product.getId(), product.getName(), product.getPrice(), product.getQuantity(), product.getPromotion());
+public record ProductResponse(
+        Long id,
+        String name,
+        int price,
+        int quantity,
+        String promotion,
+        ProductPromotionDate promotionDate
+) {
+    public ProductResponse(Product product, ProductPromotionDate date) {
+        this(
+                product.getId(),
+                product.getName(),
+                product.getPrice(),
+                product.getQuantity(),
+                product.getPromotion(),
+                date
+        );
     }
 }
