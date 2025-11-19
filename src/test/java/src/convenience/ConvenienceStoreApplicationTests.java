2a505e6838b12fa8c05ec12b00881c71ca9084a6
윤민수 <koelysa5696@naver.com>
2025-11-19 23:21:12 +0900

    chore: 기본 Application 테스트 클래스

---
+++ src/test/java/src/convenience/ConvenienceStoreApplicationTests.java
@@ -0,0 +1,12 @@
+package src.convenience;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvenienceStoreApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+
+}
