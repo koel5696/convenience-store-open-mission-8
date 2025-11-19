23f99f0e2f38d4b21b4d1d96b35114467326175e
윤민수 <koelysa5696@naver.com>
2025-11-19 23:19:47 +0900

    test: 결제 도중 선택 검증 테스트

---
+++ src/test/java/src/convenience/service/paymentService/PaymentFlowCheckerTest.java
@@ -0,0 +1,83 @@
+package src.convenience.service.paymentService;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import src.convenience.domain.cart.Cart;
+import src.convenience.dto.payment.PayRequest;
+import src.convenience.exception.MembershipException;
+import src.convenience.exception.PromotionProductException;
+
+@SpringBootTest
+@Transactional
+class PaymentFlowCheckerTest {
+
+    @Autowired
+    PaymentFlowChecker checker;
+
+    @Autowired
+    Cart cart;
+
+    @BeforeEach
+    void init() {
+        cart.clear();
+    }
+
+    private void cartSetup() {
+        cart.addItem(1L, 5);
+        cart.addItem(3L, 9);
+    }
+
+    @Test
+    void 결제중_프로모션_누락_예외_테스트() {
+        PayRequest request = new PayRequest(null, true, true);
+
+        assertThatThrownBy(() -> checker.checkFlow(request, Map.of(1L, 5)))
+                .isInstanceOf(PromotionProductException.class);
+    }
+
+
+    @Test
+    void 결제중_프로모션_누락이지만_재고까지_부족_예외_테스트() {
+        PayRequest request = new PayRequest(true, null, true);
+
+        assertThatThrownBy(() -> checker.checkFlow(request, Map.of(3L, 9)))
+                .isInstanceOf(PromotionProductException.class);
+    }
+
+    @Test
+    void 결제중_프로모션_누락과_재고_부족_동시_예외_테스트() {
+        cartSetup();
+        Map<Long, Integer> items = cart.getItems();
+        PayRequest request = new PayRequest(null, null, true);
+
+        assertThatThrownBy(() -> checker.checkFlow(request, items))
+                .isInstanceOf(PromotionProductException.class);
+    }
+
+    @Test
+    void 결제중_멤버십_미선택_예외_테스트() {
+        cartSetup();
+        Map<Long, Integer> items = cart.getItems();
+        PayRequest request = new PayRequest(true, true, null);
+
+        assertThatThrownBy(() -> checker.checkFlow(request, items))
+                .isInstanceOf(MembershipException.class);
+    }
+
+    @Test
+    void 결제중_사용자_선택_모두_완료_테스트() {
+        cartSetup();
+        Map<Long, Integer> items = cart.getItems();
+        PayRequest request = new PayRequest(true, true, true);
+
+        assertThatCode(() ->  checker.checkFlow(request, items))
+                .doesNotThrowAnyException();
+    }
+}
