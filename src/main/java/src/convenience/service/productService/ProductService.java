ce62a6da28be58d0b1bd0c2e0d8efac10bb48d85
윤민수 <koelysa5696@naver.com>
2025-11-19 22:47:00 +0900

    feat: 프로모션 기간까지 포함하여 전달하도록 구현

--- src/main/java/src/convenience/service/productService/ProductService.java
+++ src/main/java/src/convenience/service/productService/ProductService.java
@@ -3,21 +3,40 @@ package src.convenience.service.productService;
 import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import src.convenience.domain.entity.product.Product;
 import src.convenience.domain.entity.product.ProductRepository;
+import src.convenience.domain.entity.promotion.Promotion;
+import src.convenience.domain.entity.promotion.PromotionRepository;
+import src.convenience.dto.product.ProductPromotionDate;
 import src.convenience.dto.product.ProductResponse;
 
 @Service
 public class ProductService {
     private final ProductRepository productRepository;
+    private final PromotionRepository promotionRepository;
 
-    public ProductService(ProductRepository productRepository) {
+    public ProductService(ProductRepository productRepository,
+                          PromotionRepository promotionRepository) {
         this.productRepository = productRepository;
+        this.promotionRepository = promotionRepository;
     }
 
     @Transactional(readOnly = true)
     public List<ProductResponse> getAllProducts() {
         return productRepository.findAll().stream()
-                .map(ProductResponse::new)
+                .map(this::toResponse)
                 .toList();
+    }
+
+    private ProductResponse toResponse(Product product) {
+        ProductPromotionDate date = null;
+        Promotion promotion = promotionRepository.promotionCreate(product.getPromotion());
+            if (!promotion.checkNonePromotion()) {
+                date = new ProductPromotionDate(
+                        promotion.getPromotionStartDate(),
+                        promotion.getPromotionEndDate()
+                );
+            }
+        return new ProductResponse(product, date);
     }
 }
