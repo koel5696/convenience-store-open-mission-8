1d9fa1dbf597995f4bfd84c4d891dda663b48c31
윤민수 <koelysa5696@naver.com>
2025-11-19 23:08:18 +0900

    test: 상품 엔티티 테스트

---
+++ src/test/java/src/convenience/domain/entity/product/ProductTest.java
@@ -0,0 +1,51 @@
+package src.convenience.domain.entity.product;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ProductTest {
+
+    Product product;
+
+    @BeforeEach
+    void init() {
+        product = new Product("콜라", 1000, 5, "1+1");
+    }
+
+    @Test
+    void 재고_검증_성공_테스트() {
+        product.checkQuantity(5); // 예외 없어야 함
+    }
+
+    @Test
+    void 재고_검증_실패_테스트() {
+        assertThatThrownBy(() -> product.checkQuantity(6))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("재고가 부족합니다");
+    }
+
+    @Test
+    void 재고_차감_성공_테스트() {
+        product.decreaseQuantity(3);
+
+        assertThat(product.getQuantity()).isEqualTo(2);
+    }
+
+    @Test
+    void 재고_차감_실패_테스트() {
+        assertThatThrownBy(() -> product.decreaseQuantity(6))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 재고_비교_테스트() {
+        boolean result1 = product.compareQuantity(3);
+        boolean result2 = product.compareQuantity(6);
+
+        assertThat(result1).isTrue();
+        assertThat(result2).isFalse();
+    }
+}
