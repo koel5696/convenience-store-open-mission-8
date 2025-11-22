aba286f0309dc327d538703ec9dee6e1d72cf7dc
윤민수 <koelysa5696@naver.com>
2025-11-22 18:29:27 +0900

    refactor: 프로모션 상품 누락 옵션 변수명 직관적으로 수정

--- src/main/java/src/convenience/service/paymentService/PaymentFlowChecker.java
+++ src/main/java/src/convenience/service/paymentService/PaymentFlowChecker.java
@@ -3,10 +3,8 @@ package src.convenience.service.paymentService;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import src.convenience.domain.entity.product.Product;
 import src.convenience.domain.entity.product.ProductRepository;
 import src.convenience.domain.entity.promotion.Promotion;
@@ -19,6 +17,7 @@ import src.convenience.exception.PromotionProductException;
 
 @Service
 public class PaymentFlowChecker {
+    private static final int MISSED_GIFT_QUANTITY = 1;
 
     private final ProductRepository productRepository;
     private final PromotionRepository promotionRepository;
@@ -32,14 +31,14 @@ public class PaymentFlowChecker {
     public void checkFlow(PayRequest request, Map<Long, Integer> items) {
         PromotionSuggestionsResponse suggestions = analyzePromotion(items);
 
-        validateMissingPromotion(request.missingPromotion(), suggestions.upsells());
+        validateMissingPromotion(request.missingPromotion(), suggestions.missingPromotionItems());
         validateStockIssue(request.insufficientStock(), suggestions.stockIssues());
         validateMembership(request.membership());
     }
 
-    private void validateMissingPromotion(Boolean userSelection, List<PromotionOption> upsells) {
-        if (!upsells.isEmpty() && userSelection == null) {
-            throw new PromotionProductException(new PromotionSuggestionsResponse(upsells, List.of()));
+    private void validateMissingPromotion(Boolean userSelection, List<PromotionOption> missingPromotionItems) {
+        if (!missingPromotionItems.isEmpty() && userSelection == null) {
+            throw new PromotionProductException(new PromotionSuggestionsResponse(missingPromotionItems, List.of()));
         }
     }
 
@@ -56,7 +55,7 @@ public class PaymentFlowChecker {
     }
 
     private PromotionSuggestionsResponse analyzePromotion(Map<Long, Integer> items) {
-        List<PromotionOption> upsells = new ArrayList<>();
+        List<PromotionOption> missingPromotionItems = new ArrayList<>();
         List<PromotionOption> stockIssues = new ArrayList<>();
 
         items.forEach((productId, quantity) -> {
@@ -64,17 +63,18 @@ public class PaymentFlowChecker {
             Promotion promotion = promotionRepository.promotionCreate(product.getPromotion());
 
             if (promotion.isMissingPromotion(quantity)) {
-                addSuggestion(product, quantity, upsells, stockIssues);
+                addSuggestion(product, quantity, missingPromotionItems, stockIssues);
             }
         });
-        return new PromotionSuggestionsResponse(upsells, stockIssues);
+
+        return new PromotionSuggestionsResponse(missingPromotionItems, stockIssues);
     }
 
     private void addSuggestion(Product product, int quantity,
-                               List<PromotionOption> upsells, List<PromotionOption> stockIssues) {
+                               List<PromotionOption> missingPromotionItems, List<PromotionOption> stockIssues) {
 
-        if (product.compareQuantity(quantity + 1)) {
-            upsells.add(new PromotionOption(product.getName(), product.getPromotion()));
+        if (product.compareQuantity(quantity + MISSED_GIFT_QUANTITY)) {
+            missingPromotionItems.add(new PromotionOption(product.getName(), product.getPromotion()));
             return;
         }
 
