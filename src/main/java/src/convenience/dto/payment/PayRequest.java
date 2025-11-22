b94a47cee48bd5ba0f8c87a4a76aea17aec43121
윤민수 <koelysa5696@naver.com>
2025-11-22 18:30:04 +0900

    refactor: 의미없는 주석 삭제

--- src/main/java/src/convenience/dto/payment/PayRequest.java
+++ src/main/java/src/convenience/dto/payment/PayRequest.java
@@ -1,7 +1,7 @@
 package src.convenience.dto.payment;
 
 public record PayRequest(
-        Boolean missingPromotion,    // 담기 여부
-        Boolean insufficientStock, // 재고 부족 시 진행 여부
+        Boolean missingPromotion,
+        Boolean insufficientStock,
         Boolean membership
 ) {}
